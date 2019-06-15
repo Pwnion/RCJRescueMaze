501e9b243a3eddee11e6a34a7e12a52c2770187f
Zephreo <hdentry56@gmail.com>
2019-06-15 15:47:19 +1000

    boolean x6

--- src/main/java/com/pwnion/rcjrescuemaze/Main.java
+++ src/main/java/com/pwnion/rcjrescuemaze/Main.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 public class Main {
 	
 	//LIST VISITED: (Location, Distance, 4 bits for walls, 1 bit for Corner, 1 bit for Silver)
+	ArrayList<HashMap<Integer, Integer>, Boolean, Boolean, Boolean, Boolean, Boolean, Boolean> visited;
 	
 	//LIST UNVISTED: (Location)
 	ArrayList<HashMap<Integer, Integer>> unvisited;
