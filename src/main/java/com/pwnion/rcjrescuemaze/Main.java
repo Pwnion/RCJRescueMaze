3031e52ed3af649d5d9cf40edb59d2cbfcd262a8
Pwnion <aden.m@null.net>
2019-06-15 18:59:49 +1000

    Ultrasonic drivers complete

--- src/main/java/com/pwnion/rcjrescuemaze/Main.java
+++ src/main/java/com/pwnion/rcjrescuemaze/Main.java
@@ -2,14 +2,10 @@ package com.pwnion.rcjrescuemaze;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.pwnion.rcjrescuemaze.global.searching.pathing.drivers.Ultrasonic;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Injector injector = Guice.createInjector(new MainBinder());
 	}
-	
-	Ultrasonic testObject = new Ultrasonic("front");
-
 }
--- src/main/java/com/pwnion/rcjrescuemaze/global/searching/pathing/drivers/Ultrasonic.java
+++ src/main/java/com/pwnion/rcjrescuemaze/global/searching/pathing/drivers/Ultrasonic.java
@@ -1,30 +1,32 @@
 package com.pwnion.rcjrescuemaze.global.searching.pathing.drivers;
 
-import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class Ultrasonic implements Pins, GpioPinListenerDigital {
 	//Position of ultrasonic sensor to obtain distance from
-	final String position;
-	boolean echoStateChanged;
+	private final String position;
+	//Boolean that is true when the ultrasonic sensor receives the sound wave back
+	private boolean echoStateChanged;
 	
+	//Constructor that takes the position of the ultrasonic sensor on the robot as a parameter
 	public Ultrasonic(String position) {
 		this.position = position;
 	}
 	
+	//Listener for the echo pin changing states
 	@Override
 	public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
 		echoStateChanged = true;
 	}
 	
-	int getDistance() {
+	//Obtains the distance in cm from the ultrasonic sensor specified to the nearest object it detects
+	public int getDistance() {
 		sendPins.get(position).pulse(10);
 		long startTime = System.currentTimeMillis();
 		while(!echoStateChanged) {
 			if(System.currentTimeMillis() - startTime > 66); return -1;
 		}
-		return (int) (((System.currentTimeMillis() - startTime) * 1000) );
-		
+		return (int) (((System.currentTimeMillis() - startTime) * 1000) * 171.5);
 	}
 }
