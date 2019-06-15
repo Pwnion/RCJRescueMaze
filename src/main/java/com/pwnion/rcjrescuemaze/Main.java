28c21b49041c5eeef07d378b49972448700ed215
Zephreo <hdentry56@gmail.com>
2019-06-15 13:58:15 +1000

    More actual kinda code

--- src/main/java/com/pwnion/rcjrescuemaze/Main.java
+++ src/main/java/com/pwnion/rcjrescuemaze/Main.java
@@ -5,9 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		//This is probably important
 		
-		
-		//Pseudo Code:
-		//
 		//{Call upon searching function to find and move to next tile
 		//
 		//Detect for any problems in orientation or position (Mainly checks any information that may have been logged during Pathing)
--- src/main/java/com/pwnion/rcjrescuemaze/global/searching/pathing/Pathing.java
+++ src/main/java/com/pwnion/rcjrescuemaze/global/searching/pathing/Pathing.java
@@ -4,29 +4,30 @@ public class Pathing {
 	//Move from point A to B
 	//Requires Knowledge on walls and tiles visited
 	
-	private static String generatePath (int A,int B) {
+	private static String generatePath(int A,int B) {
 		//{Function: Generate Path from [A] to [B]
 		//(Insert Pathing Algorithm) Generates Path
 		//}Return [Path]
 		return Path
 	}
 	
-	private static String findDistance (int A,int B) {
+	private static int findDistance(String Path) {
 		//{Function: Find Distance using [Path]
 		//Find Distance using [Path]
 		//}Return [Distance]
 		return Distance
 	}
 
-	//{Function: Move using [Path]
-	//
-	//	{Use path to Move 1 tile (Use DrivingMotors.java {Function: Move 1 tile in [Direction]})
-	//
-	//	Log any discrepancies with rotation or position 
-	//	If over tolerance levels repathing may be required
-	//	
-	//	After moving 1 tile check for Silver tiles and update last visited silver (if required)
-	//	(This process does not happen physically and just checks Silver tile list vs Current Position)
-	//	}Repeat until end of path
-	//}
+	private static void moveByPath(String Path) {
+		//{Function: Move using [Path]
+		//
+		//	{Use path to Move 1 tile (Use DrivingMotors.java {Function: Move 1 tile in [Direction]})
+		//
+		//	Log any discrepancies with rotation or position 
+		//	If over tolerance levels repathing may be required
+		//	
+		//	After moving 1 tile check for Silver tiles and update last visited silver (if required)
+		//	(This process does not happen physically and just checks Silver tile list vs Current Position)
+		//	}Repeat until end of path
+	}
 }
