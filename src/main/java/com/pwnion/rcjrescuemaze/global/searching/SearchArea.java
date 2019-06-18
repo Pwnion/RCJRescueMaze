7ddde0975f32ad8621e835bc01244d07bb691644
Zephreo <hdentry56@gmail.com>
2019-06-18 19:29:23 +1000

    commit

--- src/main/java/com/pwnion/rcjrescuemaze/global/searching/SearchArea.java
+++ src/main/java/com/pwnion/rcjrescuemaze/global/searching/SearchArea.java
@@ -4,20 +4,37 @@ import java.util.ArrayList;
 
 import com.google.inject.Inject;
 import com.pwnion.rcjrescuemaze.SharedData;
+import com.pwnion.rcjrescuemaze.UnvisitedTileData;
+import com.pwnion.rcjrescuemaze.global.searching.pathing.drivers.Ultrasonic;
 
 public class SearchArea {
 	private final SharedData sharedData;
+	private final Ultrasonic ultrasonic;
 	
 	@Inject
-	SearchArea(SharedData sharedData) {
+	SearchArea(SharedData sharedData, Ultrasonic ultrasonic) {
 		this.sharedData = sharedData;
+		this.ultrasonic = ultrasonic;
 	}
 	
 	
 	
 	public ArrayList<Boolean> search() {
-		ArrayList<Boolean> walls = null;
+		ArrayList<Boolean> walls = new ArrayList<Boolean>();
+		
 		//Search using sensors to find walls and hence find unvisited tiles
+		for (Integer distance : ultrasonic.getDistances().values()) {
+			if (distance <= 22.5 && distance != -1) {
+				walls.add(true);
+			} else {
+				
+			}
+		}
+		
+		ultrasonic.getDistances().values().forEach((distance) -> {
+			walls.add(distance <= 22.5 && distance != -1 ? true : false);
+		});
+		
 		return walls;
 	}
 }
